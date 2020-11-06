02a2f96223c526e95ac76994092609f751f21131
Fayd <fspea123@gmail.com>
2020-11-06 18:47:31 +1000

    added some comments

--- simulator.c
+++ simulator.c
@@ -49,17 +49,19 @@ double rnd() {
     return (double) rand() / (double) RAND_MAX;
 }
 
-void step(state* s, parameters* params, int verbose) {
 
+void step(state* s, parameters* params, int verbose) {
 
     if (verbose) printf("Time Step: %d\n", s->time_step);
 
-
     person* infected[params->pop_size];
     person* susceptible[params->pop_size];
+
     int infected_count = 0;
     int susceptible_count = 0;
     int deceased_count = 0;
+
+    // Count the number of susceptible, infected and deceased.
     for (int i = 0; i < params->pop_size; i++) {
         person* p = &s->population[i];
         if (p->deceased) deceased_count++;
@@ -67,6 +69,7 @@ void step(state* s, parameters* params, int verbose) {
         else susceptible[susceptible_count++] = p;
     }
 
+    // Infect susceptible people
     const int current_infected_count = infected_count;
     for (int i = 0; i < current_infected_count; i++) {
 
@@ -76,29 +79,33 @@ void step(state* s, parameters* params, int verbose) {
 
         person* infected_person = infected[i];
 
+        // Those who have been diagnosed have a different reproduction number.
         double infection_rate = params->r0;
         if (infected_person->diagnosed) infection_rate = params->r0_diagnosed;
+
+        // A infected person remains infected for B days (recover_time).
+        // With an probability of infection of r0 / B, we can expect each
+        // infected person to infect r0 people since E[B * (r0 / B)] == r0.
         double prob_of_infection = (infection_rate / (double) params->recover_time);
 
-        if (infected_person->has_infected < infection_rate) {
-
-            // Infect new people
-            if (rnd() < prob_of_infection) {
-                person* new_infected_person = susceptible[--susceptible_count];
-                new_infected_person->infected = 1;
-                new_infected_person->has_been_infected = 1;
-                infected_person->has_infected++;
-                infected_count++;
-                //printf("Someone was infected.\n");
-            }
+        // Infect new people
+        if (rnd() < prob_of_infection) {
+            person* new_infected_person = susceptible[--susceptible_count];
+            new_infected_person->infected = 1;
+            new_infected_person->has_been_infected = 1;
+            infected_person->has_infected++;
+            infected_count++;
         }
 
     }
 
-    // Testing
+    // Each infected person has a ~test_rate~ probability of getting tested
+    // at each time. Once tested, they become diagnosed which means that
+    // person will now spread the disease with the ~r0_diagnosed~ reproduction
+    // number instead of ~r0~.
     for (int i = 0; i < params->pop_size; i++) {
         person* p = &s->population[i];
-        if (p->deceased) continue;
+        if (p->deceased || p->diagnosed) continue;
 
         if (p->infected && rnd() < params->test_rate) {
             p->diagnosed = 1;
@@ -106,21 +113,26 @@ void step(state* s, parameters* params, int verbose) {
     }
 
 
+
     for (int i = 0; i < params->pop_size; i++) {
         person* p = &s->population[i];
         if (p->deceased) continue;
 
-        // Update infection times
         if (p->infected) {
+
+            // Update infection times
             p->infected_time++;
 
+            // Each infected has a ~death_rate~ probability of
+            // dying at each timestep.
             if (rnd() < params->death_rate) {
                 p->deceased = 1;
                 deceased_count++;
                 infected_count--;
             }
 
-            // Recover if person has been infected for long enough
+            // If an infected has been infected for ~recover_time~ number of
+            // time steps, they now recover, assuming they are not deceased.
             else if (p->infected_time >= params->recover_time) {
                 p->infected = 0;
                 p->infected_time = 0;
@@ -128,7 +140,6 @@ void step(state* s, parameters* params, int verbose) {
                 p->has_infected = 0;
                 susceptible_count++;
                 infected_count--;
-                //printf("Someone recovered.\n");
             }
 
 
@@ -136,17 +147,17 @@ void step(state* s, parameters* params, int verbose) {
 
     }
 
+    // Count the totals for recovered and diagnosed.
     int recovered_count = 0;
     int diagnosed_count = 0;
     for (int i = 0; i < params->pop_size; i++) {
         person* p = &s->population[i];
         if (p->deceased) continue;
-
         if (p->has_been_infected && !p->infected) recovered_count++;
         if (p->diagnosed) diagnosed_count++;
     }
 
-
+    // Set the counts for this time step.
     s->susceptible_counts[s->time_step] = susceptible_count;
     s->infected_counts[s->time_step] = infected_count;
     s->recovered_counts[s->time_step] = recovered_count;
